7fc59a377f61df85c5d9cc5acb1d86eaaca141e8
Michl Balázs <michlbalazs0911@gmail.com>
2023-11-13 11:12:06 +0100

    started creating repository (not finished)

--- GG1RKK_HFT_2023241/GG1RKK_HFT_2023241.Repository/Generic Repository/Repository.cs
+++ GG1RKK_HFT_2023241/GG1RKK_HFT_2023241.Repository/Generic Repository/Repository.cs
@@ -3,11 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GG1RKK_HFT_2023241.Repository.Interface;
 
 namespace GG1RKK_HFT_2023241.Repository.Generic_Repository
 {
-    internal class Repository
+    public abstract class Repository<T> : IRepository<T> where T : class
     {
+        protected ShopDbContext ctx;
+        public Repository(ShopDbContext ctx)
+        {
+            this.ctx = ctx;
+        }
+        public void Create(T item)
+        {
+            ctx.Set<T>().Add(item);
+            ctx.SaveChanges();
+        }
+
+        public IQueryable<T> ReadAll()
+        {
+            return ctx.Set<T>();
+        }
+
+        public void Delete(int id)
+        {
+            ctx.Set<T>().Remove(Read(id));
+            ctx.SaveChanges();
+        }
+
+        public abstract T Read(int id);
+        public abstract void Update(T item);
 
     }
 }
---
+++ GG1RKK_HFT_2023241/GG1RKK_HFT_2023241.Repository/Interface/IRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace GG1RKK_HFT_2023241.Repository.Interface
+{
+    public interface IRepository<T> where T : class
+    {
+    }
+}
