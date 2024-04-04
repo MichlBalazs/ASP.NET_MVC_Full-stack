8297daf63c6e749815896b5ce432078e1affe3ed
Michl Balázs <michlbalazs0911@gmail.com>
2024-04-04 16:30:03 +0200

    wpf client created

---
+++ GG1RKK_HFT_2023241/GG1RKK_HFT_2023241.WpfClient/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GG1RKK_HFT_2023241.WpfClient
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
